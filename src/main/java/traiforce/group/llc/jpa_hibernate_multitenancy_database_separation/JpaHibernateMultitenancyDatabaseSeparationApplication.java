5137239857cb71ab71d0b1afc14f888abe980195
Stephen Traiforos <straiforos@nymblsystems.com>
2024-10-10 13:48:29 -0400

    Spring intializr jpa/hibernate, spring web, dev tools, configuration.

---
+++ src/main/java/traiforce/group/llc/jpa_hibernate_multitenancy_database_separation/JpaHibernateMultitenancyDatabaseSeparationApplication.java
@@ -0,0 +1,13 @@
+package traiforce.group.llc.jpa_hibernate_multitenancy_database_separation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaHibernateMultitenancyDatabaseSeparationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaHibernateMultitenancyDatabaseSeparationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/traiforce/group/llc/jpa_hibernate_multitenancy_database_separation/JpaHibernateMultitenancyDatabaseSeparationApplicationTests.java
@@ -0,0 +1,13 @@
+package traiforce.group.llc.jpa_hibernate_multitenancy_database_separation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaHibernateMultitenancyDatabaseSeparationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
